4b2bdbd5977b512796aea883d686dc9d497a9226
Henning Strandå <henninss@stud.ntnu.no>
2023-03-08 09:28:47 +0100

    added Register.java and EntityClassTest.java

---
+++ src/main/java/no/ntnu/idatt1002/demo/data/Register.java
@@ -0,0 +1,54 @@
+package no.ntnu.idatt1002.demo.data;
+
+//TODO should there be an extra class for subscriptions that inherit from Transaction?
+
+import java.util.ArrayList;
+
+public class Register {
+    private int numberOfTransactions;
+    private ArrayList<Transaction> transactions;
+
+    /**
+     * Returns the number of transactions in the register
+     *
+     * @return The number of transactions as an int
+     */
+    public int getNumberOfTransactions() {
+        return numberOfTransactions;
+    }
+
+    /**
+     * Returns a deep-copy of the transactions given a certain category
+     *
+     * @param category The category to search for as a String
+     * @return All transactions found as an ArrayList, returns an empty arraylist if none were found
+     */
+    public ArrayList<Transaction> getTransactionsByCategory(String category){
+        ArrayList<Transaction> foundTransactions = new ArrayList<>();
+        for (Transaction t : this.transactions){
+            if (t.getCategory().equals(category)){
+                Transaction transactionCopy = new Transaction(t.getAmount(), t.getCategory(), t.getName());
+                foundTransactions.add(transactionCopy);
+            }
+        }
+
+        return foundTransactions;
+    }
+
+    //TODO should this be here? Or should it only work for subscriptions
+    /**
+     * Based on all transactions in the register, this method calculates the
+     * net income
+     *
+     * @return The net income as a double. Negative means the user is losing money.
+     */
+    public double getNetIncome(){
+        double netIncome = 0;
+
+        for (Transaction t: this.transactions){
+            netIncome += t.getAmount();
+        }
+
+        return netIncome;
+    }
+}
---
+++ src/test/java/no/ntnu/idatt1002/demo/repo/EntityClassTest.java
@@ -0,0 +1,8 @@
+package no.ntnu.idatt1002.demo.repo;
+
+/**
+ * User tests for testing the entity classes Register and Entity
+ */
+public class EntityClassTest {
+
+}
