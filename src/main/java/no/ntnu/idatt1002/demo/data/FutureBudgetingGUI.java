9b44b03f388af493d7af8ed5778cfec9eaa34b87
Ole Heggum <ole.heggum@gmail.com>
2023-03-08 10:50:15 +0100

    WIP: pushing to pull central

---
+++ src/main/java/no/ntnu/idatt1002/demo/data/FutureBudgetingGUI.java
@@ -0,0 +1,62 @@
+package no.ntnu.idatt1002.demo.data;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class FutureBudgetingGUI extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  FutureBudgeting test = new FutureBudgeting(100, 95);
+
+  @Override
+  public void start(Stage primaryStage) {
+    test.initiateUnused();
+    primaryStage.setTitle("Dropdown to select category");
+
+    GridPane grid = new GridPane();
+    grid.setPadding(new Insets(10,10,10,10));
+    grid.setHgap(5);
+    grid.setVgap(5);
+
+    Text category = new Text("Category:");
+    Text expectedOut = new Text("Expected out:");
+    Text actualOut = new Text("Actual out:");
+    GridPane.setConstraints(category, 0, 0);
+    grid.getChildren().addAll(category);
+    GridPane.setConstraints(expectedOut, 1, 0);
+    grid.getChildren().addAll(expectedOut);
+    GridPane.setConstraints(actualOut, 2, 0);
+    grid.getChildren().addAll(actualOut);
+
+    for(int n = 0; n<(test.unusedCategories.size()-1); n++) {
+
+      ChoiceBox cbx = new ChoiceBox(FXCollections.observableArrayList(test.unusedCategories));
+      cbx.setValue("Choose category...");
+      GridPane.setConstraints(cbx, 0, n+1);
+      grid.getChildren().add(cbx);
+
+      TextField inputFirstCategoryExpected = new TextField();
+      inputFirstCategoryExpected.setPromptText("Enter expected output");
+      GridPane.setConstraints(inputFirstCategoryExpected, 1, n+1);
+      grid.getChildren().addAll(inputFirstCategoryExpected);
+
+      TextField inputFirstCategoryActual = new TextField("0");
+      inputFirstCategoryActual.setPromptText("Enter actual output");
+      GridPane.setConstraints(inputFirstCategoryActual, 2, n+1);
+      grid.getChildren().addAll(inputFirstCategoryActual);
+    }
+    primaryStage.setScene(new Scene(grid, 500, 400));
+    primaryStage.show();
+  }
+}
+
+
