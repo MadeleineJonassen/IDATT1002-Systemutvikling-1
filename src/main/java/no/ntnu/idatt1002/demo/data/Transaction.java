331f4c3846b5d600e5e71793197ac437bd93ba26
Henning Strandå <henninss@stud.ntnu.no>
2023-03-13 12:28:51 +0100

    added calendar support to Transaction

--- src/main/java/no/ntnu/idatt1002/demo/data/Transaction.java
+++ src/main/java/no/ntnu/idatt1002/demo/data/Transaction.java
@@ -1,6 +1,9 @@
 package no.ntnu.idatt1002.demo.data;
 
 
+import java.util.Calendar;
+//Date format to format dates
+
 /**
  *  Entity class representing a transaction in the app. All instances of this class
  *  is held by the Register class. It is assumed all amounts are the same currency
@@ -10,11 +13,39 @@ public class Transaction {
     private final double amount;
     private String category;
     private final String name;
+    private final Calendar date;
 
+    /**
+     * Constructor for a transaction, without the date. If this is called, it is assumed
+     * that the current date is the date to be set
+     *
+     * @param amount The amount of the transaction as a double. If it is negative, that means money
+     * is deducted from the user's account
+     * @param category The category of the transaction as a string
+     * @param name The name given to the transaction as a string
+     */
     Transaction(double amount, String category, String name){
         this.amount = amount;
         this.category = category;
         this.name = name;
+        this.date = Calendar.getInstance();
+    }
+
+    /**
+     * Constructor for a transaction, with a date given.
+     *
+     * @param amount The amount of the transaction as a double. If it is negative, that means money
+     * is deducted from the user's account
+     * @param category The category of the transaction as a string
+     * @param name The name given to the transaction as a string
+     * @param date The date given to the transaction as a Calendar object
+     */
+    Transaction(double amount, String category, String name, Calendar date){
+        this.amount = amount;
+        this.category = category;
+        this.name = name;
+        //TODO does this mean that if the date object passed as an argument is deleted, that this will not persist?
+        this.date = date;
     }
 
     /**
@@ -44,6 +75,13 @@ public class Transaction {
     public String getName() {
         return name;
     }
+
+    /**
+     * Returns the date for the transaction
+     *
+     * @return The date as a Calendar object
+     */
+    public Calendar getDate(){ return date; }
 
     /**
      * Changes the category associated with the transaction
