9b731e58e5a2bf96b4c85a684be3117785fd80ef
Maria Elizabeth Pauna Lane <melane@stud.ntnu.no>
2023-03-22 10:18:07 +0100

    Connects the home page with recurring transactions.

--- src/main/java/no/ntnu/idatt1002/demo/homePage/HomePageController.java
+++ src/main/java/no/ntnu/idatt1002/demo/homePage/HomePageController.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,13 +18,22 @@ public class HomePageController {
 
   private Stage stage;
   private Scene scene;
-  private Scene root;
+  private Scene rootSwitchToExpenses;
+  private AnchorPane rootSwitchToRecurringTrans;
 
   public void switchToExpenses(ActionEvent event) throws IOException {
-    root = (FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Expenses.fxml"))));
+    rootSwitchToExpenses = (FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Expenses.fxml"))));
     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     //scene = new Scene(root);
-    stage.setScene(root);
+    stage.setScene(rootSwitchToExpenses);
+    stage.show();
+  }
+
+  public void switchToRecurringTransactions(ActionEvent event) throws IOException {
+    rootSwitchToRecurringTrans = (FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/RecurringTransactions.fxml"))));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(rootSwitchToRecurringTrans);
+    stage.setScene(scene);
     stage.show();
   }
 
