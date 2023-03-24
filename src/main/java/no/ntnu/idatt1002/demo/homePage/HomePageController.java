aff83da3fd09e3f8cb2466d3aa7465cf64102e6b
Henning Strandå <henninss@stud.ntnu.no>
2023-03-24 14:00:40 +0100

    Added initialize in HomePageController to add database to recent transactions table

--- src/main/java/no/ntnu/idatt1002/demo/homePage/HomePageController.java
+++ src/main/java/no/ntnu/idatt1002/demo/homePage/HomePageController.java
@@ -3,12 +3,14 @@ package no.ntnu.idatt1002.demo.homePage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.AnchorPane;
@@ -18,9 +20,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class HomePageController {
+public class HomePageController implements Initializable {
 
+  public TableColumn transactionTable;
   private Stage stage;
   private Scene scene;
   private Scene rootSwitchToExpenses;
@@ -59,4 +63,7 @@ public class HomePageController {
     stage.show();
   }
 
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+  }
 }
