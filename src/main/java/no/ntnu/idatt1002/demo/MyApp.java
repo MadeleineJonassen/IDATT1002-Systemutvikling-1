b80fe7dcf2de745078c8a19fc84f3a42d8cc7fb8
Ole Heggum <ole.heggum@gmail.com>
2023-03-24 13:08:43 +0100

    removes unused import statement in MyApp

--- src/main/java/no/ntnu/idatt1002/demo/MyApp.java
+++ src/main/java/no/ntnu/idatt1002/demo/MyApp.java
@@ -1,6 +1,5 @@
 package no.ntnu.idatt1002.demo;
 
-import no.ntnu.idatt1002.demo.timeIntervalPicker.PeriodMain;
 import no.ntnu.idatt1002.demo.expenses.Expenses;
 import no.ntnu.idatt1002.demo.FutureBudgeting.FutureBudgetingSBVersion;
 
