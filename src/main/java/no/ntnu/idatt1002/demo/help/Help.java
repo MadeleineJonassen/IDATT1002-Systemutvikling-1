0675bc0a18412a454aa06e37932635938bdfc98f
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-03-29 08:56:33 +0200

    Add scene to Help.java

---
+++ src/main/java/no/ntnu/idatt1002/demo/help/Help.java
@@ -0,0 +1,26 @@
+package no.ntnu.idatt1002.demo.help;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Help extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    URL fxmlUrl = getClass().getResource("/HelpOption.fxml");
+    primaryStage.setTitle("Help");
+    Scene scene = FXMLLoader.load(fxmlUrl);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+
+}
+
