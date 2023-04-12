e417139ab1dddfebf7468b14b121dc4d515414d6
Maria Elizabeth Pauna Lane <melane@stud.ntnu.no>
2023-04-12 11:36:47 +0200

    Adds fetching and displaying data from json file to recurring transactions.

--- src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
+++ src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
@@ -1,64 +1,88 @@
-package no.ntnu.idatt1002.demo.recurringTransactions;
+package no.ntnu.idatt1002.demo.recurring;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import no.ntnu.idatt1002.demo.data.Expense;
 import no.ntnu.idatt1002.demo.data.Register;
+import no.ntnu.idatt1002.demo.data.RegisterController;
 import no.ntnu.idatt1002.demo.data.Transaction;
+import no.ntnu.idatt1002.demo.exceptions.ConformityException;
+import no.ntnu.idatt1002.demo.exceptions.DuplicateNameException;
 
 //import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class RecurringTransactionsController {
+public class RecurringTransactionsController implements Initializable {
   @FXML
   private Label monthName;
   @FXML
   private DatePicker datePicker;
   @FXML
-  private ListView<String> recurringTransactions;
-  private Month month;
-  private Register categoryRegister = new Register();
-
-  /**
-   * Saves the date chosen from the DatePicker and changes the month name under
-   * the pie chart.
-   */
+  private TableView<Transaction> recurringTransactions;
   @FXML
-  public void dateChosen() {
-    LocalDate date = datePicker.getValue();
-    month = date.getMonth();
-    String monthString = month.toString().toLowerCase();
-    String finalString = monthString.substring(0,1).toUpperCase() + monthString.substring(1);
-    if (date != null) {
-      monthName.setText(finalString + " overview");
-    } else {
-      monthName.setText("");
-    }
+  private TableColumn<Transaction, String> name;
+  @FXML
+  private TableColumn<Transaction, Double> amount;
+  @FXML
+  private TableColumn<Transaction, String> date;
+  @FXML
+  private PieChart pieChart;
+  private Register categoryRegister;
+
+  public RecurringTransactionsController() throws DuplicateNameException, IOException, URISyntaxException, ConformityException {
+    categoryRegister = RegisterController.readData(getClass().getClassLoader().getResource("database/register.json"));
   }
 
-  @FXML
-  public void listData() {
-    List<String> transactionStringList = new ArrayList<>();
-    List<Transaction> transactionList = categoryRegister.getTransactionsByCategoryType(true);
-    for (Transaction transaction : transactionList) {
-      transactionStringList.add(transaction.toString());
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    fillTable();
+    fillPie();
+  }
+
+  private void fillTable() {
+    ArrayList<Transaction> transactions = categoryRegister.getTransactionsByCategoryType(true);
+    ObservableList<Transaction> transactionObservableList = FXCollections.observableArrayList(transactions);
+    name.setCellValueFactory(new PropertyValueFactory<>("Name"));
+    amount.setCellValueFactory(new PropertyValueFactory<>("Amount"));
+    date.setCellValueFactory(new PropertyValueFactory<>("Date"));
+    recurringTransactions.setItems(transactionObservableList);
+  }
+
+  private void fillPie() {
+    ArrayList<Transaction> transactions = categoryRegister.getTransactionsByCategoryType(true);
+    double expenseAmount = 0;
+    double incomeAmount = 0;
+    for (Transaction transaction : transactions) {
+      if (transaction instanceof Expense) {
+        expenseAmount += transaction.getAmount();
+      } else {
+        incomeAmount += transaction.getAmount();
+      }
     }
-    ObservableList<String> observableList = FXCollections.observableList(transactionStringList);
-    recurringTransactions.setItems(observableList);
+    ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+      new PieChart.Data("Expenses", expenseAmount),
+      new PieChart.Data("Income", incomeAmount)
+    );
+    pieChart.setData(pieChartData);
   }
 
   public void goHome(ActionEvent event) throws IOException {
@@ -76,6 +100,17 @@ public class RecurringTransactionsController {
     List<String> transactionStringList = new ArrayList<>();
     transactionStringList.add("Cats");
     transactionStringList.add("Bottle");
+    ObservableList<String> observableList = FXCollections.observableList(transactionStringList);
+    recurringTransactions.setItems(observableList);
+  }
+
+   @FXML
+  public void listData() {
+    List<String> transactionStringList = new ArrayList<>();
+    List<Transaction> transactionList = categoryRegister.getTransactionsByCategoryType(true);
+    for (Transaction transaction : transactionList) {
+      transactionStringList.add(transaction.toString());
+    }
     ObservableList<String> observableList = FXCollections.observableList(transactionStringList);
     recurringTransactions.setItems(observableList);
   }
