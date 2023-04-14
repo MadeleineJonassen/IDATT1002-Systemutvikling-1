5ffe25abe9b413e9dc13785b9e7c39e610751294
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-04-14 11:56:52 +0200

    Add switching scenes to ExpensesController

--- src/main/java/no/ntnu/idatt1002/demo/expenses/ExpensesController.java
+++ src/main/java/no/ntnu/idatt1002/demo/expenses/ExpensesController.java
@@ -26,7 +26,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
-public class ExpensesController implements Initializable{
+public class ExpensesController implements Initializable {
   @FXML
   private VBox categoryCheckBoxes;
   @FXML
@@ -40,20 +40,24 @@ public class ExpensesController implements Initializable{
   @FXML
   private GridPane gridPane;
   private Register register;
+  private Stage stage;
 
-  public void editCategoryButtonPushed(){
+  private Scene scene;
+  private Scene rootSwitchToIncome;
+
+  public void editCategoryButtonPushed() {
     System.out.println("The category button has been pushed");
   }
 
-  public void editExpensesButtonPushed(){
+  public void editExpensesButtonPushed() {
     System.out.println("The edit expenses button has been pushed");
   }
 
-  public void viewIncomeButtonPushed(){
+  public void viewIncomeButtonPushed() {
     System.out.println("The view income button has been pushed");
   }
 
-  public void changeToBarGraphButtonPushed(){
+  public void changeToBarGraphButtonPushed() {
     System.out.println("The change to bar graph button has been pushed");
   }
 
@@ -68,7 +72,8 @@ public class ExpensesController implements Initializable{
 
   /**
    * Initializes the controller class.
-   * @param url The location used to resolve relative paths for the root object, or null if the location is not known.
+   *
+   * @param url            The location used to resolve relative paths for the root object, or null if the location is not known.
    * @param resourceBundle The resources used to localize the root object, or null if the root object was not localized.
    */
   public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -86,7 +91,7 @@ public class ExpensesController implements Initializable{
     try {
       register = RegisterController.readData(Objects.requireNonNull(
               getClass().getClassLoader().getResource("database/register.json")));
-    } catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
       return;
     }
@@ -119,7 +124,8 @@ public class ExpensesController implements Initializable{
         stopDate = fromDate.getValue();
         result = ("(" + startDate + "->" + stopDate + ")");
       }
-    } return result;
+    }
+    return result;
   }
 
   /**
@@ -134,7 +140,7 @@ public class ExpensesController implements Initializable{
     }
     // If the user has made a wrong selection
     // TODO: maybe make more user friendly (error message)
-    if (fromDate.getValue().isAfter(toDate.getValue())){
+    if (fromDate.getValue().isAfter(toDate.getValue())) {
       return;
     }
 
@@ -155,6 +161,15 @@ public class ExpensesController implements Initializable{
     }
 
     pieChart.setData(piechartData);
+  }
+
+  public void switchToIncome(ActionEvent event) throws IOException {
+    rootSwitchToIncome = (FXMLLoader.load(Objects.requireNonNull(
+            getClass().getResource("/Income.fxml"))));
+
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.setScene(rootSwitchToIncome);
+    stage.show();
   }
 }
 
