5f7ccc4d18a47cccbaa674c99e9ede9a58959321
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-04-18 11:15:58 +0200

    Add UX functionality to RecurringTransactions.fxml

--- src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
+++ src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
@@ -39,6 +40,8 @@ import java.util.ResourceBundle;
  * Controller for recurring transactions FXML.
  */
 public class RecurringTransactionsController implements Initializable {
+  private Stage stage;
+  private Scene scene;
   @FXML
   private Label monthName;
   @FXML
@@ -91,23 +94,6 @@ public class RecurringTransactionsController implements Initializable {
     );
     pieChart.setData(pieChartData);
   }
-
-  /**
-   * Button to go to home page of application.
-
-   * @param event when button is pressed with mouse.
-   * @throws IOException if input is invalid.
-   */
-  public void goHome(ActionEvent event) throws IOException {
-    BorderPane rootGoHome = (FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
-        "/SpendWiseHomePage.fxml"))));
-    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    Scene scene = new Scene(rootGoHome);
-    stage.setScene(scene);
-    stage.show();
-  }
-}
-
 /*
 @FXML
   public void listData() {
@@ -118,4 +104,82 @@ public class RecurringTransactionsController implements Initializable {
     recurringTransactions.setItems(observableList);
   }
 */
+
+  /**
+   * Button to go to home page of application.
+
+   * @param event when button is pressed with mouse.
+   * @throws IOException if input is invalid.
+   */
+  public void goHome(ActionEvent event) throws IOException {
+    BorderPane rootGoHome = (FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
+            "/SpendWiseHomePage.fxml"))));
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Scene scene = new Scene(rootGoHome);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * Button that takes user to income.
+
+   * @param event mouse click.
+   * @throws IOException if wrong input detected.
+   */
+  public void switchToIncome(ActionEvent event) throws IOException {
+    Scene rootSwitchToIncome = (FXMLLoader.load(Objects.requireNonNull(
+            getClass().getResource("/Income.fxml"))));
+
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    stage.setScene(rootSwitchToIncome);
+    stage.show();
+  }
+
+
+  /**
+   * Button that takes user to future budgeting.
+
+   * @param event mouse click.
+   * @throws IOException if wrong input detected.
+   */
+  public void switchToFutureBudgeting(ActionEvent event) throws IOException {
+    BorderPane rootSwitchToFutureBudgeting = (FXMLLoader.load(Objects.requireNonNull(
+            getClass().getResource("/FutureBudgeting.fxml"))));
+
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(rootSwitchToFutureBudgeting);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * Button that takes user to edit income.
+
+   * @param event mouse click.
+   * @throws IOException if wrong input detected.
+   */
+  public void switchToEditIncome(ActionEvent event) throws IOException {
+    BorderPane rootSwitchToEditIncome = (FXMLLoader.load(Objects.requireNonNull(
+            getClass().getResource("/EditIncome.fxml"))));
+
+    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(rootSwitchToEditIncome);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * A button that opens the help option menu.
+   * @param event - mouse click
+   * @throws IOException - if wrong input detected
+   */
+  public void openHelpOption(ActionEvent event) throws IOException {
+    FXMLLoader rootSwitchToHelpOption = new FXMLLoader(getClass().getResource("/HelpScenes/HelpHome.fxml"));
+    Parent rootHelp = (Parent) rootSwitchToHelpOption.load();
+    Stage stage = new Stage();
+    stage.setScene(new Scene(rootHelp));
+    stage.show();
+  }
+
+}
 
