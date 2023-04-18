4cd295b5f34bf721735f7d9c7eb0dd7bd4c03a0a
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-04-18 11:53:55 +0200

    Change HelpOption method in RecurringTransactionsController

--- src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
+++ src/main/java/no/ntnu/idatt1002/demo/recurring/RecurringTransactionsController.java
@@ -174,7 +174,7 @@ public class RecurringTransactionsController implements Initializable {
    * @throws IOException - if wrong input detected
    */
   public void openHelpOption(ActionEvent event) throws IOException {
-    FXMLLoader rootSwitchToHelpOption = new FXMLLoader(getClass().getResource("/HelpScenes/HelpHome.fxml"));
+    FXMLLoader rootSwitchToHelpOption = new FXMLLoader(getClass().getResource("/HelpScenes/HelpRecTrans.fxml"));
     Parent rootHelp = (Parent) rootSwitchToHelpOption.load();
     Stage stage = new Stage();
     stage.setScene(new Scene(rootHelp));
