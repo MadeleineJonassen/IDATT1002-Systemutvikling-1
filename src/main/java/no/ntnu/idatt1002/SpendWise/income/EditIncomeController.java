5a45cb80516f8b2364bac4aaf930b078e409be9d
MaddyPaddy2001 <madelesj@stud.ntnu.no>
2023-04-21 12:54:40 +0200

    Add EditCategories to EditIncome.fxml

--- src/main/java/no/ntnu/idatt1002/SpendWise/income/EditIncomeController.java
+++ src/main/java/no/ntnu/idatt1002/SpendWise/income/EditIncomeController.java
@@ -160,7 +160,13 @@ public class EditIncomeController implements Initializable {
     return (datePicker.getValue() == null);
   }
 
-
+  public void editCategoryButtonPushed(ActionEvent event) throws IOException {
+    FXMLLoader rootSwitchToEditCategory = new FXMLLoader(getClass().getResource("/EditCategory.fxml"));
+    Parent rootEditCategory = (Parent) rootSwitchToEditCategory.load();
+    Stage stage = new Stage();
+    stage.setScene(new Scene(rootEditCategory));
+    stage.show();
+  }
 
   /**
    * Button to go to home page.
